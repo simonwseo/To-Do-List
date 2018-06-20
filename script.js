a170656c7dd67da4edcc92891f1e25e19339c36d
Simon Seo <sseo7248@gmail.com>
2018-06-20 12:03:00 -0700

    This commit will:
    - Added comments to the js file

--- script.js
+++ script.js
@@ -32,19 +32,6 @@ var handlers = {
 let todoList = {
   todos: [],
   displayTodos: function() {
-    if (this.todos.length === 0){
-      console.log('The list is empty');
-    }else{
-      console.log('My Todos:');
-      for (let i = 0; i < this.todos.length; i++){
-
-        if (this.todos[i].completed === true){
-          console.log(' ( X ) ' + this.todos[i].todoText);
-        }else{
-          console.log(' (  ) ' + this.todos[i].todoText);
-        }
-      }
-    }
     //hides list
     if (ul.style.display == 'none'){
        displayItems.textContent = 'Hide List';
@@ -53,9 +40,8 @@ let todoList = {
         displayItems.textContent = 'Show List';
         ul.style.display = 'none';
     }
-
-
   },
+
   addTodo: function(todoText) {
 
    let todoItem = "↠" + todoText;
@@ -72,9 +58,8 @@ let todoList = {
       alert('Please enter at least one character.');
     }else{
 
-
     //Create an li element and add the input value as text content
-
+    //Also create another div to store all the buttons in
     let listItem = document.createElement('li');
     let container = document.createElement('div');
     let completedItem = document.createElement('input');
@@ -99,9 +84,10 @@ let todoList = {
     container.appendChild(deleteButton);
     container.appendChild(changeButton);
 
-
+//Reset the input value after creating the todo item
     input.value = "";
 
+//Push the newly created todo item into the array
     this.todos.push({
       todoText: todoItem,
       completed: false,
@@ -109,8 +95,8 @@ let todoList = {
     });
 
 //Change Todos
-
     changeButton.onclick = function (event){
+
 // Disables other edit buttons while editing target todo
       for (let i = 0; i < changeItem.length;i++){
         if(changeItem[i] !== event.target){
@@ -119,10 +105,11 @@ let todoList = {
         deleteItem[i].disabled = true;
       }
 
+// p is pointing to the paragraph element with the todo text
       let div = event.target.parentNode;
       let p = div.previousElementSibling;
       let previousText = p.textContent;
-      let listPosition;
+      let listPosition; // variable to store the position of the todo item
 
 
       for (let i = 0; i < todoList.todos.length; i++){
@@ -131,14 +118,17 @@ let todoList = {
         }
       }
 
+      //gets rid of the special character in the front when displayed in the placeholder
       while(previousText.charAt(0) === '↠'){
         previousText = previousText.substr(1);
       }
-
+      //when the edit button is clicked a new input will pop up for the user to type in their new text.
       p.innerHTML = '↠<input type="text" class="changeInput" placeholder=" ' + previousText + '">';
 
+      //the edit button will be hidden and cancel/submit button will be displayed
       changeItem[listPosition].style.display = 'none';
 
+    //if the buttons aren't created yet, they will be created when the change button is pressed
     if (todoList.todos[listPosition].buttonsCreated === false){
       let submitButton = document.createElement ('button');
       let cancelButton = document.createElement('button');
@@ -151,65 +141,63 @@ let todoList = {
       div.appendChild(submitButton);
       div.appendChild(cancelButton);
 
-      todoList.todos[listPosition].buttonsCreated = true;
+      todoList.todos[listPosition].buttonsCreated = true; //this prevents duplicate buttons from being created
 
-      //Submit edit button@@@@@@@@@@@@@@@@@@@@@@@@@
+      //Submit button's onclick
       submitButton.onclick = function (event){
 
-        let newTodo = '↠' + changeInput[0].value;
-        let position;
+        //uses the event.target to have variable point directly at the buttons being pressed
+        let submitButton = event.target;
+        let cancelButton = event.target.nextElementSibling;
+        let newTodo = '↠' + changeInput[0].value; //text value in the change input will be stored here
 
-        for (let i = 0; i < submitItem.length; i++){
-          if (event.target === submitItem[i]){
-            position = i;
-          }
-        }
-        p.textContent = newTodo;
-        todoList.changeTodo(listPosition,newTodo);
+        p.textContent = newTodo; //Replaces the previous todo item with edited version
+        todoList.changeTodo(listPosition,newTodo); //updates the array
 
+        //enables the other buttons
         for (let i = 0; i < changeItem.length;i++){
           if(changeItem[i] !== event.target){
             changeItem[i].disabled = false;
             deleteItem[i].disabled = false;
           }
         }
-      changeItem[listPosition].style.display = 'inline-block';
-      cancelItem[position].style.display = 'none';
-      submitItem[position].style.display = 'none';
+
+      changeItem[listPosition].style.display = 'inline-block'; //brings back the change button
+      // hides the submit and cancel buttons
+      cancelButton.style.display = 'none';
+      submitButton.style.display = 'none';
+      // add button is enabled
       addList.disabled = false;
       }
 
-      //Cancel edit button@@@@@@@@@@@@@@@
+      //Cancel edit button
         cancelButton.onclick = function (event){
 
+          //same as submit button
           let cancelButton = event.target;
           let submitButton = event.target.previousElementSibling;
 
+          //when canceled, the previous text will be maintained
+          p.innerHTML = todoList.todos[listPosition].todoText; //grabs the previous text from array
 
-
-          // let position;
-          //
-          // for (let i = 0; i < cancelItem.length; i++){
-          //   if (event.target === cancelItem[i]){
-          //     position = i;
-          //   }
-          // }
-          p.innerHTML = todoList.todos[listPosition].todoText;
-
+          //enables the change and delete buttons
           for (let i = 0; i < changeItem.length;i++){
             if(changeItem[i] !== event.target){
               changeItem[i].disabled = false;
               deleteItem[i].disabled = false;
             }
           }
+
+        //brings back change button and hide the submit & cancel buttons
         changeItem[listPosition].style.display = 'inline-block';
         cancelButton.style.display = 'none';
         submitButton.style.display = 'none';
+        //add button also enabled
         addList.disabled = false;
         }
 
     }else{
-      debugger;
+      //if the list item already has submit & cancel button, it just reveals them instead of creating new ones
       let submitItem = event.target.nextElementSibling;
       let cancelItem = submitItem.nextElementSibling;
 
@@ -227,18 +215,20 @@ let todoList = {
         let todoItem = div.parentNode;
         let position;
 
+      //grabs the position of the delete button
       for (let i = 0; i < todoList.todos.length; i++){
         	if (event.target === deleteItem[i]){
             position = i;
           }
         }
-
+      //Asks the user if they want to delete the item, if yes then delete from list/array
         if (confirm ('Are you sure you want to delete this item?')){
         todoList.deleteTodo(position);
         ul.removeChild(todoItem);
       }else{
         //Do nothing
       }
+      //enables the add button
        addList.disabled = false;
       }
 
@@ -268,14 +258,14 @@ let todoList = {
 
 
   changeTodo: function(targetNum, content) {
-  this.todos[targetNum].todoText = content;
+  this.todos[targetNum].todoText = content; //changing array item's text content
   },
   deleteTodo: function(itemNumber){
-    this.todos.splice(itemNumber, 1);
+    this.todos.splice(itemNumber, 1); //removing array item's text content
   },
   toggleCompleted: function(position){
     let todo = this.todos[position];
-    todo.completed = !todo.completed;
+    todo.completed = !todo.completed; //toggles incomplete/complete
   },
 
   toggleAll: function(){
@@ -283,12 +273,14 @@ let todoList = {
     let totalTodos = this.todos.length;
     let completedTodos = 0;
 
-    for (let i = 0; i < totalTodos; i++){
-      if (this.todos[i].completed === true){
+   //counting how many completed todos there are
+    this.todos.forEach(function (todo){
+      if (todo.completed === true){
         completedTodos++;
       }
-    }
+    });
 
+    //if everything is completed then toggle all incomplete
     if (totalTodos === completedTodos){
          for(let i = 0; i < totalTodos; i++){
            this.todos[i].completed = false;
@@ -299,6 +291,7 @@ let todoList = {
            li[i].style.textDecoration = 'none';
 
          }
+      //if opposite, toggle everything completed
        }else{
          for(let i = 0; i < this.todos.length; i++){
            this.todos[i].completed = true;
@@ -315,12 +308,14 @@ let todoList = {
    deleteAll: function(){
      let todo = this.todos;
 
+     //clears out the whole list & the array
      if (confirm ('Are you sure you want to delete everything?')){
        ul.innerHTML = "";
        todo.splice(0,todo.length);
        }else{
          //Do nothing
        }
+     //if user was in middle of editing, add list would be disabled, so enable it when using deleteAll
        addList.disabled = false;
     },
 
@@ -328,16 +323,18 @@ let todoList = {
     clearAllChecked: function(){
       let allClear = 0;
 
+      // counts how many list items are checked as completed
       for (let i = 0; i < checkBox.length; i++){
         if (checkBox[i].checked === true){
           allClear++;
         }
       }
 
+      // if everything is checked then delete everything in ul/array
       if (allClear === checkBox.length){
         ul.innerHTML = "";
         this.todos.splice(0,this.todos.length);
-      }else if (checkBox.length > 0){
+      }else{ // if not, delete just the checked items
         for (let i = 0; i < checkBox.length; i++){
          for (let i = 0; i < checkBox.length; i++){
             if (checkBox[i].checked === true){
@@ -351,5 +348,7 @@ let todoList = {
         }
   }
  }
+
+ addList.disabled = false;
 }
 };
